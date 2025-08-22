d416370cd183de51fbea617290f84cc960e4d18e
DiandreRuiz <Diandreruiz77@gmail.com>
2025-08-21 22:25:17 -0400

    Light refactor

--- app.js
+++ app.js
@@ -96,7 +96,6 @@ function initializeButtonListeners() {
     nextQuestionButton.addEventListener("click", () => {
         advanceQuiz();
     });
-    ``;
 }
 
 async function displayQuestionGetAnswer() {
